34611ec14c5813d8f6965b00fa06a44eb54d1e98
Alessandro Camillo <dinosparky@gmail.com>
2014-05-15 15:49:27 -0300

    animacaozinha

--- jogo-lixo/MyScene.h
+++ jogo-lixo/MyScene.h
@@ -10,4 +10,19 @@
 
 @interface MyScene : SKScene
 
+@property SKSpriteNode *lixeiraMetal;
+@property SKSpriteNode *lixeiraPapel;
+@property SKSpriteNode *lixeiraVidro ;
+@property SKSpriteNode *lixeiraPlastico;
+
+@property SKSpriteNode *lixo1;
+@property SKSpriteNode *lixo2;
+@property SKSpriteNode *lixo3;
+@property SKSpriteNode *lixo4;
+@property SKSpriteNode *lixo5;
+@property SKSpriteNode *lixo6;
+
+@property SKNode *lixos;
+
+
 @end
--- jogo-lixo/MyScene.m
+++ jogo-lixo/MyScene.m
@@ -14,36 +14,104 @@
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
         
-        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
+        self.backgroundColor = [SKColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0];
         
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+        [self setLixeiraMetal: [SKSpriteNode spriteNodeWithColor: [UIColor yellowColor] size:CGSizeMake(30, 50)]];
+        [self lixeiraMetal].position = CGPointMake(75, self.frame.size.height/2 +180);
+        [self setLixeiraPapel: [SKSpriteNode spriteNodeWithColor:[UIColor blueColor] size:CGSizeMake(30, 50)]];
+        [self lixeiraPapel].position = CGPointMake(75, self.frame.size.height/2 +60);
+        [self setLixeiraVidro: [SKSpriteNode spriteNodeWithColor:[UIColor greenColor] size:CGSizeMake(30, 50)]];
+        [self lixeiraVidro].position = CGPointMake(75, self.frame.size.height/2 -60);
+        [self setLixeiraPlastico: [SKSpriteNode spriteNodeWithColor:[UIColor redColor] size:CGSizeMake(30, 50)] ];
+        [self lixeiraPlastico].position = CGPointMake(75, self.frame.size.height/2 -180);
         
-        myLabel.text = @"Hello, World!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
+        [self addChild: [self lixeiraMetal]];
+        [self addChild: [self lixeiraPapel]];
+        [self addChild: [self lixeiraVidro]];
+        [self addChild: [self lixeiraPlastico]];
         
-        [self addChild:myLabel];
+        
+        [self setLixo1: [SKSpriteNode spriteNodeWithColor:[UIColor grayColor] size:CGSizeMake(30, 50)]];
+        [self lixo1].position = CGPointMake(self.frame.size.width/2 + 75, self.frame.size.height/2 +300);
+        [self setLixo2: [SKSpriteNode spriteNodeWithColor:[UIColor grayColor] size:CGSizeMake(30, 50)]];
+        [self lixo2].position = CGPointMake(self.frame.size.width/2 + 75, self.frame.size.height/2 +180);
+        [self setLixo3: [SKSpriteNode spriteNodeWithColor:[UIColor grayColor] size:CGSizeMake(30, 50)]];
+        [self lixo3].position = CGPointMake(self.frame.size.width/2 + 75, self.frame.size.height/2 +60);
+        [self setLixo4: [SKSpriteNode spriteNodeWithColor:[UIColor grayColor] size:CGSizeMake(30, 50)]];
+        [self lixo4].position = CGPointMake(self.frame.size.width/2 + 75, self.frame.size.height/2 -60);
+        [self setLixo5: [SKSpriteNode spriteNodeWithColor:[UIColor grayColor] size:CGSizeMake(30, 50)]];
+        [self lixo5].position = CGPointMake(self.frame.size.width/2 + 75, self.frame.size.height/2 -180);
+        [self setLixo6: [SKSpriteNode spriteNodeWithColor:[UIColor grayColor] size:CGSizeMake(30, 50)]];
+        [self lixo6].position = CGPointMake(self.frame.size.width/2 + 75, self.frame.size.height/2 -300);
+        
+        
+        [self setLixos: [[SKNode alloc] init]];
+        
+        [[self lixos] addChild: [self lixo1]];
+        [[self lixos] addChild: [self lixo2]];
+        [[self lixos] addChild: [self lixo3]];
+        [[self lixos] addChild: [self lixo4]];
+        [[self lixos] addChild: [self lixo5]];
+        [[self lixos] addChild: [self lixo6]];
+        
+        [self addChild: [self lixos]];
+        
+        [self animaLixosTeste: [NSNumber numberWithInt:1]];
+        
     }
     return self;
+}
+
+-(void)animaLixos{
+    [[self lixos] runAction: [SKAction moveBy:CGVectorMake(0, -150) duration:2]];
+
+}
+
+
+
+-(void)animaLixosTeste: (NSNumber* )animacao{
+    
+    switch ([animacao intValue]) {
+        case 1:
+            [[self lixos] runAction: [SKAction moveBy:CGVectorMake(0, -100) duration:2]];
+            break;
+        case 2:
+            [[self lixos] runAction: [SKAction moveBy:CGVectorMake(-100, 0) duration:2]];
+            break;
+        case 3:
+            [[self lixos] runAction: [SKAction moveBy:CGVectorMake(0, 100) duration:2]];
+            break;
+        case 4:
+            [[self lixos] runAction: [SKAction moveBy:CGVectorMake(100, 0) duration:2]];
+            break;
+    }
+    [[self lixo1] runAction: [SKAction rotateByAngle:-(M_PI/2) duration:0.3]];
+    [[self lixo2] runAction: [SKAction rotateByAngle:-(M_PI/2) duration:0.3]];
+    [[self lixo3] runAction: [SKAction rotateByAngle:-(M_PI/2) duration:0.3]];
+    [[self lixo4] runAction: [SKAction rotateByAngle:-(M_PI/2) duration:0.3]];
+    [[self lixo5] runAction: [SKAction rotateByAngle:-(M_PI/2) duration:0.3]];
+    [[self lixo6] runAction: [SKAction rotateByAngle:-(M_PI/2) duration:0.3]];
+    
+    animacao = [NSNumber numberWithInt:[animacao intValue] + 1];
+    if ([animacao intValue] > 4) {
+        animacao = [NSNumber numberWithInt:1];
+    }
+    [self performSelector:@selector(animaLixosTeste:) withObject:animacao afterDelay:2];
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
     
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
-    }
+//    for (UITouch *touch in touches) {
+//        CGPoint location = [touch locationInNode:self];
+//        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
+//        sprite.position = location;
+//        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
+//        [sprite runAction:[SKAction repeatActionForever:action]];
+//        [self addChild:sprite];
+//    }
+
+    
 }
 
 -(void)update:(CFTimeInterval)currentTime {
